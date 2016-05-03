9e6fda80d26f80586dda43af75e1776ff7b8c909
Amel Nurkic <domace-matrice@hotmail.com>
2016-05-03 10:58:39 +0200

    Pocetak

---
+++ src/paket/Konverter.java
@@ -0,0 +1,42 @@
+package paket;
+
+public class Konverter {
+
+	// kg u funte
+	public static double kgToPounds(double kg) {
+		double pounds = kg * 2.20462262;
+		return pounds;
+	}
+
+	// pounds to kg
+	public static double poundsToKg(double pounds) {
+		double kg = pounds * 0.45359237;
+		return kg;
+	}
+
+	// cm to inch
+	public static double cmToInch(double cm) {
+		return 0;
+	}
+
+	// inch to cm
+	public static double inchToCm(double inch) {
+		return 0;
+	}
+
+	// celsius to fahrenheit
+	public static double celToFahr(double C) {
+		return 0;
+	}
+
+	// celsius to fahrenheit
+	public static double FahrToCel(double F) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
